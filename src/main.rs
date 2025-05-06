4d6a93910889219286b900a72b6bebaf5a9b1cf3
Khalid Zubair <khalid.zubair@gmail.com>
2025-05-06 14:57:46 +0200

    Add main function to initialize application with environment configuration and logging

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use env_logger;
+use anyhow;
+use actix_web::web;
+
+use buraq::config::AppConfig; 
+
+
+#[actix_web::main]
+async fn main() -> Result<(), anyhow::Error> {
+    dotenvy::dotenv()?;
+    env_logger::init();  
+
+    let app_config = AppConfig::from_env()?; 
+
+    let _app_data = web::Data::new(
+        app_config 
+    );
+    
+    
+    Ok(())
+}
