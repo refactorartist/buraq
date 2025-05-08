e105581edff4fa0bcb043640cd1597c7dd057c9a
Khalid Zubair <khalid.zubair@gmail.com>
2025-05-08 14:38:26 +0200

    Add repository structure and ProjectRepository implementation
    Introduced a new base repository trait for CRUD operations with MongoDB, and implemented the ProjectRepository for managing Project documents. This includes methods for creating, reading, updating, deleting, and finding projects, along with unit tests to ensure functionality and reliability.

---
+++ src/repositories/base.rs
@@ -0,0 +1,55 @@
+use mongodb::Collection;
+use mongodb::bson::{Document, doc};
+use mongodb::bson::oid::ObjectId;
+use anyhow::Error;
+use mongodb::results::{InsertOneResult, UpdateResult, DeleteResult};
+use serde::Serialize;
+use serde::de::DeserializeOwned;
+use futures::TryStreamExt;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Repository<T: Send + Sync + Serialize + DeserializeOwned + 'static> {
+    async fn create(&self, item: T) -> Result<InsertOneResult, Error> {
+        let collection = self.collection()?;
+        let result = collection.insert_one(item).await?;
+        Ok(result)
+    }
+
+    async fn read(&self, id: ObjectId) -> Result<Option<T>, Error> {
+        let collection = self.collection()?;
+        let filter = doc! { "_id": id };
+        let result = collection.find_one(filter).await?;
+        Ok(result)
+    }
+
+    async fn update(&self, id: ObjectId, item: T) -> Result<UpdateResult, Error> {
+        let collection = self.collection()?;
+        let filter = doc! { "_id": id };
+        let result = collection.replace_one(filter, item).await?;
+        Ok(result)
+    }
+
+    async fn delete(&self, id: ObjectId) -> Result<DeleteResult, Error> {
+        let collection = self.collection()?;
+        let filter = doc! { "_id": id };
+        let result = collection.delete_one(filter).await?;
+        Ok(result)
+    }
+
+    async fn find(&self, filter: Document) -> Result<Vec<T>, Error> {
+        let collection = self.collection()?;
+
+        let mut cursor = collection.find(filter).await?; 
+        let mut results = Vec::new();
+
+        while let Some(item) = cursor.try_next().await? {
+            results.push(item);
+        }
+
+        Ok(results) 
+    }
+
+    fn collection(&self) -> Result<Collection<T>, Error>;
+}
+
