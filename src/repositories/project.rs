e8b5cdb4fa18a1362d1d9f9f5925f1d43f4ac8a7
Khalid Zubair <khalid.zubair@gmail.com>
2025-05-10 04:48:31 +0200

    Update database names in project and service_account repositories for clarity and specificity

--- src/repositories/project.rs
+++ src/repositories/project.rs
@@ -74,7 +74,7 @@ mod tests {
         let app_config = AppConfig::from_env(Some(true))?;
 
         let client = create_database_client(&app_config.application.database_uri).await?;
-        let db = client.database("test_db");
+        let db = client.database("test_db__projects");
         Ok(db)
     }
 
