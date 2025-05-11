122e2ffd915d7d375ef75bd380dc43fac972bffb
padawont <ryanharriszubair@gmail.com>
2025-05-11 13:49:53 +0200

    added serviceaccountservice (not done)

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,6 +1,4 @@
 pub mod project_service; 
-<<<<<<< HEAD
-pub mod service_account_service;
-=======
 pub mod environment_service;
->>>>>>> e4f0477faccf70cea51df859ee77c25d430225f6
+pub mod service_account_service;
+
--- src/services/service_account_service.rs
+++ src/services/service_account_service.rs
@@ -1,5 +1,8 @@
-use crate::{models::service_account::{self, ServiceAccount}, repositories::service_account::ServiceAccountRepository};
-use mongodb::Database;
+use crate::{
+    models::service_account::{self, ServiceAccount},
+    repositories::{base::Repository, service_account::ServiceAccountRepository},
+};
+use mongodb::{Database, error::Error};
 use serde::{Deserialize, Serialize};
 
 pub struct ServiceAccountService {
@@ -34,27 +37,55 @@ impl Into<mongodb::bson::Document> for ServiceAccountFilter {
 }
 
 impl ServiceAccountService {
-    pub async  fn new(db: Database) -> Self {
+    pub async fn new(db: Database) -> Self {
         let service_account_service = ServiceAccountRepository::new(db).unwrap();
         Self {
             service_account_repository: service_account_service,
         }
     }
 
+    pub async fn create_service_account_service(
+        &self,
+        service_account: ServiceAccount,
+    ) -> Result<ServiceAccount, Error> {
+        let result = self
+            .service_account_repository
+            .create(service_account.clone())
+            .await?;
+        let id = result.inserted_id.as_object_id().unwrap();
 
-    pub async fn create_service_account_service(&self,service_account:ServiceAccount) -> Result<ServiceAccount,Error> {
-        let 
-        
+        let insert_service_account = self
+            .service_account_repository
+            .read(id)
+            .await?
+            .ok_or_else(|| Error::msg("Failed to fetch created service_account"));
 
+        Ok(insert_service_account)
     }
-
-
-
-
-
-
 }
 
+#[cfg(test)]
+
+mod tests {
+
+    use super::*;
+    use crate::{
+        models::service_account,
+        services::service_account_service,
+        test_utils::{cleanup_test_db, setup_test_db},
+    };
+    use tokio;
+
+    async fn setup_service_account_for_filter_tests(
+        service_account_service: &ServiceAccountService,
+    ) -> Result<(), Error> {
+    let collation = service_account_service.service_account_repository.collection()?;
+    let db = collation.client().database(&collation.namespace().db);
+    cleanup_test_db(db).await?;
 
 
+    let service_account1 = ServiceAccount::new()
 
+
+    }
+}
