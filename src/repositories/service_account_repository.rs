15a60d3ee0be3aa59c56d623376df4325e1c8e53
Khalid Zubair <khalid.zubair@gmail.com>
2025-05-25 14:09:53 +0200

    Refactor pagination handling in multiple repositories to utilize new skip and limit methods from Pagination struct

--- src/repositories/service_account_repository.rs
+++ src/repositories/service_account_repository.rs
@@ -87,8 +87,8 @@ impl Repository<ServiceAccount> for ServiceAccountRepository {
         }
 
         if let Some(p) = pagination {
-            options.skip = Some(((p.page - 1) * p.limit) as u64);
-            options.limit = Some(p.limit as i64);
+            options.skip = Some(p.skip());
+            options.limit = Some(p.limit());
         }
 
         let result = self
